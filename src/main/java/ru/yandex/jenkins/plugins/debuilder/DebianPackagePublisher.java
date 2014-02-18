bb21cd342d0a45e6cad02f41de80dcfff4c6758d
Caio Cezar <raizdepi@gmail.com>
2014-02-18 20:36:38 -0300

    Added the republish option with force distribution, to promote packages; Added the option to ignore the install process of some dependencies;

--- src/main/java/ru/yandex/jenkins/plugins/debuilder/DebianPackagePublisher.java
+++ src/main/java/ru/yandex/jenkins/plugins/debuilder/DebianPackagePublisher.java
@@ -11,7 +11,9 @@ import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.Descriptor;
 import hudson.model.Project;
+import hudson.model.Run.Artifact;
 import hudson.plugins.git.GitSCM;
+import hudson.remoting.VirtualChannel;
 import hudson.scm.SCM;
 import hudson.scm.SubversionSCM;
 import hudson.tasks.BuildStepDescriptor;
@@ -24,27 +26,32 @@ import hudson.util.ListBoxModel;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.io.Serializable;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import jenkins.model.ArtifactManager;
 import jenkins.model.Jenkins;
 import net.sf.json.JSONObject;
 
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.commons.lang3.text.StrSubstitutor;
+import org.apache.commons.lang3.tuple.Pair;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 import org.tmatesoft.svn.core.auth.ISVNAuthenticationProvider;
 
 import ru.yandex.jenkins.plugins.debuilder.DebUtils.Runner;
-import ru.yandex.jenkins.plugins.debuilder.dpkg.DpkgException;
+import ru.yandex.jenkins.plugins.debuilder.dpkg.DebianChanges;
+import ru.yandex.jenkins.plugins.debuilder.dpkg.common.DebianDistributions;
+import ru.yandex.jenkins.plugins.debuilder.dpkg.common.DebianFileEntry;
 import ru.yandex.jenkins.plugins.debuilder.dupload.Dupload;
 import ru.yandex.jenkins.plugins.debuilder.dupload.DuploadException;
 import ru.yandex.jenkins.plugins.debuilder.dupload.DuploadFTPMethod;
@@ -56,6 +63,8 @@ public class DebianPackagePublisher extends Recorder implements Serializable {
     private String repoId;
     private String commitMessage;
     private final boolean commitChanges;
+    private final boolean republish;
+    private String republishDistribution;
 
     /**
      * Constructor with the required fields that jenkins require
@@ -68,10 +77,13 @@ public class DebianPackagePublisher extends Recorder implements Serializable {
      *            Condition required to commit the changes
      */
     @DataBoundConstructor
-    public DebianPackagePublisher(String repoId, String commitMessage, boolean commitChanges) {
+    public DebianPackagePublisher(String repoId, String commitMessage, boolean commitChanges, boolean republish,
+            String republishDistribution) {
         this.commitChanges = commitChanges;
         this.commitMessage = commitMessage;
         this.repoId = repoId;
+        this.republish = republish;
+        this.republishDistribution = republishDistribution;
 
         if (getRepo() == null) {
             throw new IllegalArgumentException(MessageFormat.format("Repo {0} is not found in global configuration", repoId));
@@ -189,41 +201,132 @@ public class DebianPackagePublisher extends Recorder implements Serializable {
     public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws IOException {
         PrintStream logger = listener.getLogger();
 
-        if (build.getResult().isWorseThan(Result.SUCCESS)) {
-            logger.println(MessageFormat.format(DebianPackageBuilder.ABORT_MESSAGE, PREFIX,
-                    "Build is not success, will not execute debrelease"));
-            return true;
-        }
-
         Runner runner = new DebUtils.Runner(build, launcher, listener, PREFIX);
 
-        DebianPackageRepo repo = getRepo();
+        List<String> modules = new ArrayList<String>();
 
-        List<String> builtModules = new ArrayList<String>();
+        if (republish) {
+            boolean someChangesFound = false;
 
-        // Get all build steps of Debian Build Package
-        for (BuildBadgeAction action : build.getBadgeActions()) {
-            if (action instanceof DebianBadge) {
-                builtModules.add(((DebianBadge) action).getModule());
+            AbstractBuild<?, ?> targetBuild = build.getRootBuild();
+            ArtifactManager artManager = targetBuild.getArtifactManager();
+
+            // Get artifacts
+            @SuppressWarnings("rawtypes")
+            Map<String, Artifact> artifacts = new HashMap<String, Artifact>();
+            for (@SuppressWarnings("rawtypes")
+            Artifact artifact : targetBuild.getArtifacts()) {
+                artifacts.put(artifact.getFileName(), artifact);
+            }
+
+            // Get all .changes files
+            for (String artName : artifacts.keySet()) {
+                if (artName.endsWith(".changes")) {
+
+                    runner.announce("Republishing " + artName);
+                    someChangesFound = true;
+
+                    FilePath tmpDir = createTmpDir(build.getWorkspace());
+
+                    FilePath changesFile = copyArtifact(artifacts.get(artName), artManager, tmpDir);
+
+                    DebianChanges changes = new DebianChanges();
+                    List<Pair<String, String>> rejected = changes.parse(changesFile);
+
+                    if (rejected == null) {
+                        runner.announce("Fail reading the .changes file");
+                        return false;
+                    } else if (!rejected.isEmpty()) {
+                        runner.announce("Some lines from .changes are invalid.");
+                        for (Pair<String, String> line : rejected)
+                            runner.announce(line.getLeft() + ": " + line.getRight());
+                    }
+
+                    if (changes.getFiles() == null || changes.getFiles().getFileEntries() == null) {
+                        runner.announce("None files in .changes");
+                        return false;
+                    }
+
+                    Iterator<DebianFileEntry> iFiles = changes.getFiles().getFileEntries().iterator();
+                    while (iFiles.hasNext()) {
+                        DebianFileEntry entry = iFiles.next();
+
+                        if (!artifacts.containsKey(entry.getName())) {
+                            runner.announce("File " + entry.getName() + " not found in artifacts, removing from .changes");
+                            iFiles.remove();
+                            continue;
+                        }
+
+                        if (copyArtifact(artifacts.get(entry.getName()), artManager, tmpDir) == null)
+                            runner.announce("Fail copying " + entry.getName());
+                    }
+
+                    if (republishDistribution != null && !republishDistribution.isEmpty()) {
+                        DebianDistributions dist = new DebianDistributions();
+                        if (!dist.add(republishDistribution)) {
+                            runner.announce("Fail forcing the ditribution");
+                            return false;
+                        }
+                        changes.setDistributions(dist);
+                    }
+
+                    try {
+                        changesFile.write(changes.getBody(), "UTF-8");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        runner.announce("Fail rewriting the .changes");
+                    }
+                    modules.add(tmpDir.getRemote());
+                }
+            }
+
+            // upload
+            if (!someChangesFound) {
+                runner.announce("None .changes file found in artifacts");
+                return false;
+            }
+
+        } else {
+
+            if (build.getResult().isWorseThan(Result.SUCCESS)) {
+                logger.println(MessageFormat.format(DebianPackageBuilder.ABORT_MESSAGE, PREFIX,
+                        "Build is not success, will not execute debrelease"));
+                return true;
+            }
+
+            Map<String, DebianBadge> badges = new HashMap<String, DebianBadge>();
+
+            // Get all badges from this build. (A badge is a mark when the was
+            // successful)
+            for (BuildBadgeAction action : build.getBadgeActions()) {
+                if (action instanceof DebianBadge) {
+                    badges.put(((DebianBadge) action).getModule(), (DebianBadge) action);
+                }
+            }
+
+            // Get the path in the remote machine from all debian build steps
+            Collection<String> allRemoteModules = DebianPackageBuilder.getRemoteModules(build);
+
+            if (allRemoteModules.size() <= 0) {
+                runner.announce("None module found!");
+            }
+
+            for (String remoteModule : allRemoteModules) {
+                // If a module was not built, skip
+                String builtModule = new FilePath(build.getWorkspace().getChannel(), remoteModule).child("debian").getRemote();
+                if (!badges.containsKey(builtModule)) {
+                    runner.announce("Module in {0} was not built - not releasing", remoteModule);
+                    continue;
+                }
+                modules.add(remoteModule);
             }
         }
 
-        // Get all registered Debian Build Package steps
-        Collection<String> allRemoteModules = DebianPackageBuilder.getRemoteModules(build);
-
-        if (allRemoteModules.size() <= 0) {
-            runner.announce("None module found!");
-        }
+        DebianPackageRepo repo = getRepo();
 
         try {
-            for (String module : allRemoteModules) {
+            for (String module : modules) {
                 runner.announce("Publishing package from '" + module + "'");
-
-                // If a module was not built, skip
-                if (!builtModules.contains(new FilePath(build.getWorkspace().getChannel(), module).child("debian").getRemote())) {
-                    runner.announce("Module in {0} was not built - not releasing", module);
-                    continue;
-                }
 
                 // Get the publish success
                 boolean wereBuilds = false;
@@ -235,7 +338,8 @@ public class DebianPackagePublisher extends Recorder implements Serializable {
 
                     String duploadConfPath = "/etc/dupload.conf";
 
-                    runner.runCommand("sudo apt-get -y install dupload devscripts");
+                    if (!getDescriptor().isDontInstallTools())
+                        runner.runCommand("sudo apt-get -y install dupload devscripts");
                     generateDuploadConf(duploadConfPath, build, runner);
 
                     if (!runner.runCommandForResult("cd ''{0}'' && debrelease", module))
@@ -245,15 +349,31 @@ public class DebianPackagePublisher extends Recorder implements Serializable {
 
                 } else if (repo.getMethod().equals("ftp")) {
 
-                    FilePath dotChangeFolder = new FilePath(build.getWorkspace().getChannel(), module).getParent();
+                    FilePath modulePath = new FilePath(build.getWorkspace().getChannel(), module);
+
+                    FilePath dotChangeBasePath = modulePath.getParent();
+                    FilePath changelog = modulePath.child("debian/changelog");
 
                     DuploadFTPMethod duploadFTP = new DuploadFTPMethod(repo.getFqdn());
                     duploadFTP.setPassword(repo.getPassword());
                     duploadFTP.setUsername(repo.getLogin());
 
                     // TODO execute the upload process on the slave machine
-                    Dupload.start(dotChangeFolder, duploadFTP, repo.getIncoming(), "$distribution$", runner);
+                    if (!republish)
+                        Dupload.start(changelog, dotChangeBasePath, duploadFTP, repo.getIncoming(), "$distribution$", runner);
+                    else
+                        Dupload.start(modulePath.list("*.changes")[0], duploadFTP, repo.getIncoming(), "$distribution$", runner);
+
                     wereBuilds = true;
+                }
+
+                if (republish) {
+                    for (String tmpDir : modules)
+                        if (!removeTmpDir(build.getWorkspace().getChannel(), tmpDir))
+                            runner.announce("Fail removing tmpdir " + tmpDir);
+
+                    // If republishing skip final publish process
+                    continue;
                 }
 
                 if (wereBuilds && commitChanges) {
@@ -268,14 +388,8 @@ public class DebianPackagePublisher extends Recorder implements Serializable {
         } catch (DebianizingException e) {
             logger.println(MessageFormat.format(DebianPackageBuilder.ABORT_MESSAGE, PREFIX, e.getMessage()));
             build.setResult(Result.UNSTABLE);
-        } catch (DpkgException e) {
-            logger.println(MessageFormat.format(DebianPackageBuilder.ABORT_MESSAGE, PREFIX, e.getMessage()));
-            build.setResult(Result.UNSTABLE);
         } catch (DuploadException e) {
             logger.println(MessageFormat.format(DebianPackageBuilder.ABORT_MESSAGE, PREFIX, e.getMessage()));
-            build.setResult(Result.UNSTABLE);
-        } catch (Exception e) {
-            logger.println(MessageFormat.format(DebianPackageBuilder.ABORT_MESSAGE, PREFIX, ExceptionUtils.getStackTrace(e)));
             build.setResult(Result.UNSTABLE);
         }
 
@@ -355,6 +469,8 @@ public class DebianPackagePublisher extends Recorder implements Serializable {
 
         private List<DebianPackageRepo> repos = new ArrayList<DebianPackageRepo>();
 
+        private boolean dontInstallTools;
+
         public DescriptorImpl() {
             super();
             load();
@@ -385,6 +501,7 @@ public class DebianPackagePublisher extends Recorder implements Serializable {
         @Override
         public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
             repos = req.bindJSONToList(DebianPackageRepo.class, formData.get("repositories"));
+            setDontInstallTools(formData.getBoolean("dontInstallTools"));
             save();
 
             return super.configure(req, formData);
@@ -400,6 +517,14 @@ public class DebianPackagePublisher extends Recorder implements Serializable {
         public String getDisplayName() {
             return "Publish debian packages";
         }
+
+        public boolean isDontInstallTools() {
+            return dontInstallTools;
+        }
+
+        public void setDontInstallTools(boolean dontInstallTools) {
+            this.dontInstallTools = dontInstallTools;
+        }
     }
 
     public boolean isCommitChanges() {
@@ -412,5 +537,100 @@ public class DebianPackagePublisher extends Recorder implements Serializable {
 
     public String getRepoId() {
         return repoId;
+    }
+
+    public boolean isRepublish() {
+        return republish;
+    }
+
+    public String getRepublishDistribution() {
+        return republishDistribution;
+    }
+
+    public void setRepublishDistribution(String republishDistribution) {
+        this.republishDistribution = republishDistribution;
+    }
+
+    /**
+     * @param file
+     * @return The file copied or null if it fail.
+     */
+    @SuppressWarnings("rawtypes")
+    private FilePath copyArtifact(Artifact artifact, ArtifactManager manager, FilePath path) {
+        if (artifact == null || manager == null || path == null)
+            return null;
+
+        InputStream input = null;
+        try {
+            input = manager.root().child(artifact.relativePath).open();
+
+            FilePath dest = path.child(artifact.getFileName());
+            dest.copyFrom(input);
+
+            return dest;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (input != null)
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+    }
+
+    /**
+     * @param path
+     * @return The tmp path or null
+     */
+    private FilePath createTmpDir(FilePath path) {
+        if (path == null)
+            return null;
+
+        try {
+            return path.createTempDir("debian-package-builder.", ".republish");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * @param path
+     * @return True if removed false if not
+     */
+    private boolean removeTmpDir(FilePath path) {
+        if (path == null)
+            return false;
+
+        try {
+            path.deleteRecursive();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * @param channel
+     *            The channel
+     * @param path
+     *            the remote path
+     * @return Same as {@link #removeTmpDir(FilePath)}
+     */
+    private boolean removeTmpDir(VirtualChannel channel, String path) {
+        return removeTmpDir(new FilePath(channel, path));
     }
 }
