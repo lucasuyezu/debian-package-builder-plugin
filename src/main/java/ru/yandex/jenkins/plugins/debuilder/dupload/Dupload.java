2b2f18d80edb132f1bdbf84c10e3f74e1b82fa9a
Caio Cezar <raizdepi@gmail.com>
2014-02-18 21:01:49 -0300

    Added some exceptions, that will be removed in the new version; Now the upload process only upload the files for the current version;

--- src/main/java/ru/yandex/jenkins/plugins/debuilder/dupload/Dupload.java
+++ src/main/java/ru/yandex/jenkins/plugins/debuilder/dupload/Dupload.java
@@ -4,15 +4,75 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.tuple.Pair;
+
+import ru.yandex.jenkins.plugins.debuilder.DebianizingException;
 import ru.yandex.jenkins.plugins.debuilder.DebUtils.Runner;
 import ru.yandex.jenkins.plugins.debuilder.dpkg.DebianChanges;
-import ru.yandex.jenkins.plugins.debuilder.dpkg.DpkgException;
-import ru.yandex.jenkins.plugins.debuilder.dupload.DuploadControl.ActionType;
-import ru.yandex.jenkins.plugins.debuilder.dupload.DuploadControl.ControlEntry;
+import ru.yandex.jenkins.plugins.debuilder.dpkg.common.DebianDistributions;
+import ru.yandex.jenkins.plugins.debuilder.dpkg.common.DebianFileEntry;
+import ru.yandex.jenkins.plugins.debuilder.dupload.control.ActionType;
+import ru.yandex.jenkins.plugins.debuilder.dupload.control.ControlEntry;
+import ru.yandex.jenkins.plugins.debuilder.dupload.control.DuploadControl;
 import hudson.FilePath;
 
 public class Dupload {
+
+    public static void start(FilePath changelog, FilePath dotChangesBasePath, DuploadUploadMethod uploadMethod,
+            String incomingPath, String distributionPattern, Runner runner) throws DuploadException, IOException,
+            InterruptedException {
+        // TODO parse change log
+        // TODO validate architectures type 'one arch', source, multiarch
+
+        Map<String, String> changelogFields = parseChangelog(runner, changelog.getParent().getRemote());
+
+        String dotChangesFilePattern = changelogFields.get("Source") + "_" + changelogFields.get("Version") + "_*.changes";
+        boolean found = false;
+        for (FilePath dotChangesFile : dotChangesBasePath.list(dotChangesFilePattern)) {
+            found = true;
+            start(dotChangesFile, uploadMethod, incomingPath, distributionPattern, runner);
+        }
+        if (!found)
+            runner.announce("None .changes file found to publish");
+    }
+
+    private static Map<String, String> parseChangelog(Runner runner, String remoteDebian) throws DuploadException {
+        String changelogOutput;
+        try {
+            changelogOutput = runner.runCommandForOutput("cd \"{0}\" && dpkg-parsechangelog -lchangelog", remoteDebian);
+        } catch (DebianizingException e) {
+            e.printStackTrace();
+            throw new DuploadException("Fail parsing changelog.");
+        }
+
+        Map<String, String> changelog = new HashMap<String, String>();
+        Pattern changelogFormat = Pattern.compile("(\\w+):\\s*(.*)");
+
+        for (String row : changelogOutput.split("\n")) {
+            Matcher matcher = changelogFormat.matcher(row);
+            if (matcher.matches()) {
+                changelog.put(matcher.group(1), matcher.group(2));
+            }
+        }
+
+        if (changelog.isEmpty())
+            throw new DuploadException("Fail parsing changelog. Original input: " + changelogOutput);
+
+        String[] fields = { "Source", "Version", "Distribution", "Urgency", "Maintainer", "Date", "Changes" };
+
+        for (String field : fields) {
+            if (changelog.get(field) == null)
+                throw new DuploadException("Fail parsing changelog, '" + field + "' not found. Original input: "
+                        + changelogOutput);
+        }
+
+        return changelog;
+    }
 
     /**
      * Initialize the dupload process
@@ -34,63 +94,61 @@ public class Dupload {
      *             For errors with file operations
      * @throws InterruptedException
      *             If file operations where interrupted
-     * @throws DpkgException
+     * @throws DebianChangesParseException
+     * @throws DebianChangesValidateException
      */
-    public static void start(FilePath dotChangesFolder, DuploadUploadMethod uploadMethod, String incomingPath,
-            String distributionPattern, Runner runner) throws DuploadException, IOException, InterruptedException, DpkgException {
+    public static void start(FilePath changesFile, DuploadUploadMethod uploadMethod, String incomingPath,
+            String distributionPattern, Runner runner) throws DuploadException, IOException, InterruptedException {
 
-        if (uploadMethod == null || dotChangesFolder == null || incomingPath == null || runner == null)
+        if (uploadMethod == null || changesFile == null || incomingPath == null || runner == null)
             throw new DuploadException("Dupload failed, invalid parameters");
 
-        FilePath[] dotChangeFiles = dotChangesFolder.list("*.changes");
+        if (!changesFile.exists())
+            throw new DuploadException("The required '.changes' file (" + changesFile.getName() + ") does not exists");
+        else if (changesFile.isDirectory())
+            throw new DuploadException("The required '.changes' file (" + changesFile.getName() + ") is a directory");
 
-        if (dotChangeFiles.length <= 0)
-            throw new DuploadException("None '.changes' file found!");
+        runner.announce("Validating '" + changesFile.getRemote() + "'");
+        DebianChanges debianChanges = new DebianChanges();
 
-        for (FilePath dotChangeFile : dotChangeFiles) {
-            if (dotChangeFile.isDirectory())
-                continue;
+        String error = debianChanges.read(changesFile);
 
-            runner.announce("Processing '" + dotChangeFile.getRemote() + "'");
-            DebianChanges debianChanges = new DebianChanges();
+        if (!error.isEmpty())
+            throw new DuploadException("Dupload failed validating '" + changesFile.getRemote() + "'\n" + error);
 
-            if (!debianChanges.readDotChangesFile(dotChangeFile, runner))
-                throw new DuploadException("Dupload failed validating '" + dotChangeFile.getRemote() + "'");
+        FilePath dotUploadFile = new FilePath(changesFile.getParent(), changesFile.getBaseName() + ".upload");
 
-            FilePath dotUploadFile = new FilePath(dotChangeFile.getParent(), dotChangeFile.getBaseName() + ".upload");
+        // Remove from upload queue files that has already been uploaded
+        ArrayList<FilePath> filesToUpload = getFilesToUpload(dotUploadFile, changesFile, debianChanges, runner);
 
-            // Remove from upload queue files that has already been uploaded
-            ArrayList<FilePath> filesToUpload = getFilesToUpload(dotUploadFile, dotChangeFile, debianChanges, runner);
+        if (filesToUpload == null || filesToUpload.isEmpty()) {
+            runner.announce("All files has already been uploaded.");
+        } else {
+            HashMap<FilePath, String> localAndRemoteFilesDestination = generateUploadPaths(filesToUpload, incomingPath,
+                    distributionPattern, debianChanges.getDistributions());
 
-            if (filesToUpload == null || filesToUpload.isEmpty()) {
-                runner.announce("All files has already been uploaded.");
-            } else {
-                HashMap<FilePath, String> localAndRemoteFilesDestination = generateUploadPaths(filesToUpload, incomingPath,
-                        distributionPattern, debianChanges.getDistributions());
-
-                if (!uploadMethod.storeFile(localAndRemoteFilesDestination, runner))
-                    throw new DuploadException("Dupload failed uploading files");
-            }
-
-            try {
-                // TODO register uploaded files even though some has failures
-                registerControlFile(dotUploadFile, dotChangeFile.getName(), filesToUpload, uploadMethod.getServer());
-            } catch (DuploadException e) {
-                e.printStackTrace();
-                throw new DuploadException("Fail registering de control: " + e.getLocalizedMessage());
-            }
-
+            if (!uploadMethod.storeFile(localAndRemoteFilesDestination, runner))
+                throw new DuploadException("Dupload failed uploading files");
         }
-        // TODO test if this distribution is valid
+
+        try {
+            // TODO register uploaded files even though some has failures
+            registerControlFile(dotUploadFile, changesFile.getName(), filesToUpload, uploadMethod.getServer());
+        } catch (DuploadException e) {
+            e.printStackTrace();
+            throw new DuploadException("Fail registering de control: " + e.getLocalizedMessage());
+        }
+
+        // TODO test if this distribution and architecture is valid
         // TODO search for extra announce files *.announce
     }
 
     /**
      * The all file names that was not already uploaded
      * 
-     * @param dotUploadFile
+     * @param uploadFile
      *            The .upload control file
-     * @param dotChangesFile
+     * @param changesFile
      *            The .changes file
      * @param debianChanges
      *            The debian changes
@@ -101,39 +159,42 @@ public class Dupload {
      *             If some file operation fail
      * @throws InterruptedException
      *             If Some file operation is interrupted
-     * @throws DuploadException
      */
-    private static ArrayList<FilePath> getFilesToUpload(FilePath dotUploadFile, FilePath dotChangesFile,
-            DebianChanges debianChanges, Runner runner) throws IOException, InterruptedException, DuploadException {
+    private static ArrayList<FilePath> getFilesToUpload(FilePath uploadFile, FilePath changesFile, DebianChanges debianChanges,
+            Runner runner) throws IOException, InterruptedException {
 
         ArrayList<FilePath> fileUploadQueue = new ArrayList<FilePath>();
         ArrayList<String> filesInControl = new ArrayList<String>();
 
-        if (dotUploadFile != null && dotUploadFile.exists() && !dotUploadFile.isDirectory()) {
-            // Read the control file
-            DuploadControl control = new DuploadControl(dotUploadFile, runner);
+        // Read the control file
+        DuploadControl control = new DuploadControl();
+        List<Pair<String, String>> rejected = control.read(uploadFile);
 
-            // Get all uploaded files in the control
-            for (DuploadControl.ControlEntry fileControlEntry : control.getEntries()) {
-                for (DuploadControl.ActionType action : fileControlEntry.getActions()) {
-                    if (action.getActionType() == DuploadControl.ActionType.UPLOAD_ACTION) {
-                        filesInControl.add(fileControlEntry.getFileName());
-                        continue;
-                    }
-                }
+        if (rejected == null) {
+            runner.announce("Error reading .upload file");
+        } else {
+            if (rejected.size() > 0) {
+                runner.announce("Some lines from .upload file were rejected");
+                for (Pair<String, String> pair : rejected)
+                    runner.announce(pair.getLeft() + ": " + pair.getRight());
             }
 
+            // Get all uploaded files in the control
+            for (ControlEntry controlEntry : control.getEntries())
+                for (ActionType action : controlEntry.getActions())
+                    if (action.getActionType() == ActionType.UPLOAD_ACTION)
+                        filesInControl.add(controlEntry.getFileName());
         }
 
-        FilePath basePath = dotChangesFile.getParent();
-        for (String changesFileEntry : debianChanges.getFiles().keySet())
+        FilePath basePath = changesFile.getParent();
+        for (DebianFileEntry changesEntry : debianChanges.getFiles().getFileEntries())
             // If the file was not already uploaded add to upload queue
-            if (!filesInControl.contains(changesFileEntry))
-                fileUploadQueue.add(basePath.child(changesFileEntry));
+            if (!filesInControl.contains(changesEntry.getName()))
+                fileUploadQueue.add(basePath.child(changesEntry.getName()));
 
         // If the .changes was not uploaded add to queue
-        if (!filesInControl.contains(dotChangesFile.getName()))
-            fileUploadQueue.add(dotChangesFile);
+        if (!filesInControl.contains(changesFile.getName()))
+            fileUploadQueue.add(changesFile);
 
         return fileUploadQueue;
     }
@@ -153,7 +214,7 @@ public class Dupload {
      * @return
      */
     private static HashMap<FilePath, String> generateUploadPaths(ArrayList<FilePath> files, String remoteBasePath,
-            String distributionPattern, String[] distributions) {
+            String distributionPattern, DebianDistributions distributions) {
 
         remoteBasePath = remoteBasePath == null ? "" : remoteBasePath + "/";
 
@@ -164,12 +225,12 @@ public class Dupload {
 
         ArrayList<String> dists = new ArrayList<String>();
         if ((distributionPattern != null && !distributionPattern.isEmpty())
-                && (distributions != null && distributions.length > 0)) {
-            for (String dist : distributions)
+                && (distributions != null && distributions.size() > 0)) {
+            for (String dist : distributions.getCopy())
                 if (!dist.isEmpty())
                     dists.add(dist);
         } else {
-            // Dummy dist
+            // Dummy distribution
             dists.add("");
         }
 
@@ -214,27 +275,21 @@ public class Dupload {
 
                     ControlEntry fileEntry;
 
-                    fileEntry = new ControlEntry(changeFileEntry.getName(), new ActionType(
-                            DuploadControl.ActionType.UPLOAD_ACTION, destinationServer, now));
+                    fileEntry = new ControlEntry(changeFileEntry.getName(), new ActionType(ActionType.UPLOAD_ACTION,
+                            destinationServer, now));
 
                     control.mergeEntry(fileEntry);
                 }
             }
 
-            ControlEntry changesFileEntry = new ControlEntry(debianChangesFileName, new ActionType(
-                    DuploadControl.ActionType.SUCCESS_ACTION, destinationServer, now));
+            ControlEntry changesFileEntry = new ControlEntry(debianChangesFileName, new ActionType(ActionType.SUCCESS_ACTION,
+                    destinationServer, now));
             control.mergeEntry(changesFileEntry);
 
             DuploadControl.addControlLines(dotUploadFile, control);
         } catch (DuploadException e) {
             e.printStackTrace();
             throw new DuploadException("Error create a control object: " + e.getLocalizedMessage());
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new DuploadException("Error writing control: " + e.getLocalizedMessage());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            throw new DuploadException("Error writing control: " + e.getLocalizedMessage());
         }
     }
 }
